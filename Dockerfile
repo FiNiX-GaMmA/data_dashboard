a356e32d4a6e420cc47f07075220923df6639862
Aryaroop Majumder <136429863+aryaroopmajumder@users.noreply.github.com>
2024-02-28 10:38:01 +0530

    innitial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# app/Dockerfile
+
+FROM python:3.9-slim
+LABEL authors="AryaroopMajumder"
+
+RUN python3 -m pip install --no-cache-dir --upgrade \
+    pip \
+    setuptools \
+    wheel
+
+COPY . /var/www
+
+RUN pip3 install -r requirements.txt
+
+EXPOSE 8007
+
+HEALTHCHECK CMD curl --fail http://localhost:8007/_stcore/health
+
+
+ENTRYPOINT ["streamlit", "run", "streamlit.py", "--server.port=8007", "--server.address=0.0.0.0"]
---
+++ streamlit.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+st.set_page_config(layout="wide")
+
+@st.cache_data
+def load_data(path: str):
+    data = pd.read_csv(path)
+    # cleaning process can be done here
+    return data
+
+
+if __name__ == "__main__":
+    st.title("Data Dashboard")
+
+
+    with st.sidebar:
+        st.title("Configuration")
+        uploaded_file = st.sidebar.file_uploader("Upload the file")
+
+    if uploaded_file is not None:
+        df = load_data(uploaded_file)
+        with st.expander("Data preview (Click to expand)"):
+            st.dataframe(df)
+    else:
+        st.info("No file uploaded. Please upload the file")
+        st.stop()
